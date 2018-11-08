f05f7ba33e7507f81758608124d7f6198ae8bacb
Nicolas Ivanov <Nicolas Ivanov>
2018-11-07 20:04:30 -0500

    Initial get PSD feature function.

---
+++ src/feat_ext/getSpectralPowerFeatures.m
@@ -0,0 +1,8 @@
+function feat_vector = getSpectralPowerFeatures(x,f,Fs)
+% Function calculates the power spectral density in decibels
+% of a given signal different frequencies
+%   x  - Vector containing the signal of interest
+%   f  - Vector containing the frequencies at which to calculate the PSD
+%   Fs - The sampling frequency
+
+feat_vector = 10*log10(pwelch(x,[],[],f,Fs));
